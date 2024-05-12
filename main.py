0ecd2c713ae3d03914e9d2089aea2d8a266f6bb7
dawidCempulik <cempulikdawid@gmail.com>
2024-05-12 10:05:02 +0200

    Basic redist connection

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+import redis
+import os
+
+redist_host = os.environ.get("REDIST_HOST")
+redist_port = os.environ.get("REDIST_PORT")
+redist_password = os.environ.get("REDIST_PASSWORD")
+
+if redist_host is None or redist_port is None or redist_password is None:
+    print("Environment variables not set. Aborting!")
+    exit()
+
+
+r = redis.Redis(
+  host=redist_host,
+  port=redist_port,
+  password=redist_password,
+  decode_responses=True)
+
+r.set("foo", "bar")
+print(r.get("foo"))
